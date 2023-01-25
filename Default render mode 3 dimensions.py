faa2ac93bedfabaf0120f2770d6f7e1401f1ba77
Karla <120686295+KarlaPrz02@users.noreply.github.com>
2023-01-25 23:34:04 +0100

    Add files via upload
    A default render mode config file for overviewer that includes overworld, nether and end render options.

---
+++ Default render mode 3 dimensions.py
@@ -0,0 +1,26 @@
+worlds["ssp"] = "C:\\Users\\imcar\\Downloads\\world"
+
+renders["ssp-overworld"] = {
+    "world": "ssp",
+    "title": "Overworld - north",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld",
+    "northdirection" : "upper-left",
+}
+
+renders["ssp-nether"] = {
+    "world": "ssp",
+    "title": "Nether - north",
+    "rendermode": "nether",
+    "dimension": "nether",
+    "northdirection" : "upper-left",
+}
+
+renders["ssp-end"] = {
+    "world": "ssp",
+    "title": "End - north",
+    "dimension": "end",
+    "northdirection" : "upper-left",
+}
+
+outputdir = "C:\\Users\\imcar\\Desktop\\test2"
